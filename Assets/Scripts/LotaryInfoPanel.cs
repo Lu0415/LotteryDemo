a78fb9a2c02fd95dea5d483df4b37dd8ebf02cf6
盧彥辰 <cyril@hyper-light.net>
2021-06-18 17:11:39 +0800

    git: Init Fist

---
+++ Assets/Scripts/LotaryInfoPanel.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LotaryInfoPanel : MonoBehaviour
+{
+
+    //抽獎資訊父物體
+    public Transform lotaryInfoTran;
+    private Transform[] lotaryInfoArr;
+    private LotteryInfoCell[] lotaryInfoCellArr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (LotteryInfoController.SharedInstance.canUpdateInfo) {
+            LotteryInfoController.SharedInstance.canUpdateInfo = false;
+            for (int i = 0; i < lotaryInfoTran.childCount; i++)
+            {
+                lotaryInfoCellArr[i].SetItemCount(count: LotteryInfoController.SharedInstance.rewardInfoCountArr[i]);
+                Debug.Log("LotteryInfoController.SharedInstance.rewardInfoCountArr " + i + ": " + LotteryInfoController.SharedInstance.rewardInfoCountArr[i].ToString());
+            }
+            
+        }
+    }
+
+    public void Init()
+    {
+
+        lotaryInfoArr = new Transform[lotaryInfoTran.childCount];
+        lotaryInfoCellArr = new LotteryInfoCell[lotaryInfoTran.childCount];
+
+        for (int i = 0; i < lotaryInfoTran.childCount; i++)
+        {
+            // 資訊
+            lotaryInfoArr[i] = lotaryInfoTran.GetChild(i);
+            lotaryInfoCellArr[i] = lotaryInfoArr[i].GetComponent<LotteryInfoCell>();
+            lotaryInfoCellArr[i].SetItemTitle(title: LotteryInfoController.SharedInstance.rewardArr[i]);
+        }
+
+        Debug.Log("lotaryInfoCellArr: " + lotaryInfoCellArr.Length);
+        Debug.Log("");
+    }
+}
---
+++ Assets/Scripts/LotteryCell.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LotteryCell : MonoBehaviour
+{
+    public Transform[] turnEff;
+    public Transform[] seletEff;
+    public Text[] titleText;
+
+    public enum EffType
+    {
+        turn,
+        select,
+        all,
+    }
+
+    public void SetTitle(string title)
+    {
+        for (int i = 0; i < titleText.Length; i++)
+        {
+
+            titleText[i].text = title;
+        }
+
+
+
+    }
+
+    public void ShowEff(EffType efftype, bool isShow)
+    {
+
+        switch (efftype)
+        {
+            case EffType.turn:
+                for (int i = 0; i < turnEff.Length; i++)
+                {
+
+                    turnEff[i].gameObject.SetActive(isShow);
+                }
+                break;
+            case EffType.select:
+                for (int i = 0; i < turnEff.Length; i++)
+                {
+
+                    seletEff[i].gameObject.SetActive(isShow);
+                }
+                break;
+            case EffType.all:
+                for (int i = 0; i < turnEff.Length; i++)
+                {
+                    turnEff[i].gameObject.SetActive(isShow);
+                    seletEff[i].gameObject.SetActive(isShow);
+                }
+                break;
+            default:
+                break;
+        }
+
+
+
+    }
+
+    public void HideAllEff()
+    {
+        for (int i = 0; i < turnEff.Length; i++)
+        {
+
+            turnEff[i].gameObject.SetActive(false);
+        }
+        for (int i = 0; i < turnEff.Length; i++)
+        {
+
+            seletEff[i].gameObject.SetActive(false);
+        }
+    }
+
+  
+
+    IEnumerator HideEffAni()
+    {
+        yield return new WaitForSeconds(0.1f);
+        for (int i = 0; i < turnEff.Length; i++)
+        {
+
+            turnEff[i].gameObject.SetActive(false);
+        }
+    }
+}
